885c2e40e138a1f19050da763f567cbd58a51cc3
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2022-05-01 09:42:33 +0100

    changelog added. fixes #2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mair_remote",
-    version="0.1",
+    version="0.1.0",
     description="MAIR Remote Python API",
     packages=["mair"],
     install_requires=["python-osc"],
