ed397e57aa2a9e39dfac786ece356b0134bc6c6e
onyx-and-iris <code@onyxandiris.online>
2024-02-16 12:26:24 +0000

    XAirRemoteConnectionTimeoutError added to errors

--- xair_api/errors.py
+++ xair_api/errors.py
@@ -1,2 +1,14 @@
 class XAirRemoteError(Exception):
     """Base error class for XAIR Remote."""
+
+
+class XAirRemoteConnectionTimeoutError(XAirRemoteError):
+    """Exception raised when a connection attempt times out"""
+
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
+        super().__init__(
+            f"Timeout attempting to connect to mixer at {self.ip}:{self.port}"
+        )
