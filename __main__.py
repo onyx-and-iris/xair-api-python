fd4a2280861fcf4d4b0c9ff80f6b86aa82812fba
Onyx and Iris <code@onyxandiris.online>
2025-01-15 11:10:45 +0000

    re-run through ruff formatter

--- __main__.py
+++ __main__.py
@@ -2,16 +2,16 @@ import xair_api
 
 
 def main():
-    kind_id = "XR18"
-    ip = "<ip address>"
+    kind_id = 'XR18'
+    ip = '<ip address>'
 
     with xair_api.connect(kind_id, ip=ip) as mixer:
-        mixer.strip[8].config.name = "sm7b"
+        mixer.strip[8].config.name = 'sm7b'
         mixer.strip[8].config.on = True
         print(
-            f"strip 09 ({mixer.strip[8].config.name}) has been set to {mixer.strip[8].config.on}"
+            f'strip 09 ({mixer.strip[8].config.name}) has been set to {mixer.strip[8].config.on}'
         )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
